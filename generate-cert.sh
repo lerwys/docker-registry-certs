77de498109a4dbe211b0ff7fd2ba7fa5c1787d89
Lucas Russo <lerwys@gmail.com>
2019-04-04 17:39:44 -0300

    generate-cert.sh: bump expiration date to 980 years!

--- generate-cert.sh
+++ generate-cert.sh
@@ -4,4 +4,4 @@ mkdir -p certs
 
 openssl req \
     -newkey rsa:4096 -nodes -sha256 -keyout certs/domain.key \
-    -x509 -days 365 -out certs/domain.crt
+    -x509 -days 358000 -out certs/domain.crt
